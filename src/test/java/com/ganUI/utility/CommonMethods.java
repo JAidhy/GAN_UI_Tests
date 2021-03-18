f6a8c2156f954b29b288e51b2db43ed2fd820a48
jaidhy <jaspreetaidhy@gmail.com>
2021-03-18 13:44:44 +0000

    Added explicit wait method

--- src/test/java/com/ganUI/utility/CommonMethods.java
+++ src/test/java/com/ganUI/utility/CommonMethods.java
@@ -1,6 +1,10 @@
 package com.ganUI.utility;
 
+import com.ganUI.BaseTest;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CommonMethods {
 
@@ -9,4 +13,23 @@ public class CommonMethods {
         webElement.click();
 
     }
-}
+
+    //Method for explicit wait of a WebElement
+    public boolean waitForWebElement(WebElement element, int time){
+        boolean elementPresent;
+        try {
+
+            WebDriverWait wait = new WebDriverWait(BaseTest.getBaseClassInstance().getDriver(), time);
+            wait.until(ExpectedConditions.visibilityOf(element));
+            elementPresent = element.isDisplayed();
+            return elementPresent;
+        }
+        catch(Exception e){
+            elementPresent = false;
+            System.out.println(e.getMessage());
+            return elementPresent;
+        }
+        }
+
+    }
+
